efef85d4581adcdd308030d8de5bc1e88b5c684f
Lena Ringstad Olsen <lena.ringstad.olsen@helse-nord.no>
2025-05-10 09:57:07 +0200

    Fikset datofiltrering på datadump

--- dev/oppsett.R
+++ dev/oppsett.R
@@ -1,6 +1,5 @@
 
 Sys.setenv(FALK_EXTENDED_USER_RIGHTS= "[{\"A\":85,\"R\":\"SC\",\"U\":601161},{\"A\":85,\"R\":\"LU\",\"U\":601161},{\"A\":85,\"R\":\"LU\",\"U\":108172},{\"A\":85,\"R\":\"LU\",\"U\":103575}]")
-Sys.setenv(R_RAP_INSTANCE="QAC")
 Sys.setenv(R_RAP_CONFIG_PATH="/home/rstudio/nakke/data-raw/config")
 # Sys.setenv(MYSQL_DB_DATA="DegenNakkeReportDataStaging") #"db_data"
 Sys.setenv(MYSQL_DB_DATA="nakke") #"db_data"
